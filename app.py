74f443a52e658f291106aacbe34b0b17eeb176d3
GyorgySandor <sgyorgy91@gmail.com>
2019-05-22 10:24:46 +0200

    files initialised, get and post requests routed

--- app.py
+++ app.py
@@ -2,29 +2,27 @@ from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-get_counter = 0
-post_counter = 0
+
 
 
 @app.route('/')
+@app.route('/index')
 def home_page():
     return render_template('index.html')
 
 
-@app.route('/request_counter', methods=['GET, POST'])
+@app.route('/request_counter', methods=['GET', 'POST'])
 def request_counter():
-    if request.method == 'GET':
-        get_counter += 1
-        return redirect('/')
-    else:
-        post_counter += 1
-
-    return redirect('/')
+    if request.method == 'POST':
+        print("POST")
+        return redirect('/index')
+    print("GET")
+    return redirect('/index')
 
 
 @app.route('/statistics')
 def request_statistics():
-    return render_template('statistics.html', get_counter=get_counter, post_counter=post_counter)
+    return render_template('statistics.html')
 
 
 if __name__ == '__main__':
